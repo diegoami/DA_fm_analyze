e254a221220f3b294b28c1bcebbe83e33b37efb4
Diego Amicabile <diego.amicabile@gmail.com>
2023-04-13 06:52:33 +0200

    Do with bilbao

--- view_parse.py
+++ view_parse.py
@@ -3,10 +3,11 @@ import os
 
 
 def parse_selection(basedir):
+    if not os.path.isfile(f'{basedir}/csel.csv'):
+        return None
+
     with open(f'{basedir}/csel.txt', 'r', encoding='UTF-8') as file:
         raw_data = file.readlines()
-
-
     columns = raw_data[0].strip().strip('|').split('|')
     columns = [x.strip() for x in columns]
     cleaned_data = []
@@ -31,13 +32,24 @@ def parse_selection(basedir):
     df.drop(columns=["Name"], inplace=True, axis=1)
     return df
 
+
+def deal_with_fuzzy_attrs(field):
+    if '-' in field:
+        first, lst = field.split('-')
+        if first.isdigit() and lst.isdigit():
+            return round((int(first) + int(lst)) / 2)
+        else:
+            return field
+    else:
+        return field
+
 def parse_attr_list(basedir):
     if os.path.isfile(f'{basedir}/all_attrs.csv'):
         df = pd.read_csv(f'{basedir}/all_attrs.csv')
         return df
     else:
         with open(f'{basedir}/all_attrs.txt', 'r', encoding='UTF-8') as file:
-            raw_data = file.readlines()
+            raw_data = [line for line in file.readlines() if line.strip() ]
         print(raw_data)
 
         columns = raw_data[0].strip().strip('|').split('|')
@@ -50,6 +62,7 @@ def parse_attr_list(basedir):
             line = line.strip().replace(" - Pick Player ", "").strip('|')
 
             cleaned_line = [x.strip() for x in line.split('|')]
+            cleaned_line = [deal_with_fuzzy_attrs(x) for x in cleaned_line]
             cleaned_data.append(cleaned_line)
         df = pd.DataFrame(cleaned_data, columns=columns)
         for col in columns[1:]:
